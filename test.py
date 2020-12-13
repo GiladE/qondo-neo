25fbf3bed2b56c8ee0450033d9b8a7f526904f20
Gilad Even-Tov <gilad@eventov.com>
2020-12-13 18:05:36 +0200

    Add test cases todo

--- test.py
+++ test.py
@@ -16,5 +16,19 @@ class FlaskTestCase(unittest.TestCase):
     response = tester.get('/api/')
     self.assertEqual(response.status_code, 200)
 
+# Posting a csv containing tweets (unique-id,tweet-body) to /api/tweets should return http 201
+
+# Posting a csv containing tweets (unique-id,tweet-body) to /api/tweets should create tweets in the db
+
+# Posting a csv containing tweets (unique-id,tweet-body) to /api/tweets should not create duplicates
+
+# Making a get request to /api/tweets should return http 200
+
+# Making a get request to /api/tweets should return an unanswered sentiment
+
+# Posting to /api/answers/<tweet-id> with a correct tweet-id should return 201
+
+# Posting to /api/answers/<tweet-id> with a correct tweet-id should return next sentiment to be answered
+
 if __name__ == '__main__':
   unittest.main()
