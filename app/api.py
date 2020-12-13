553890ba814114f1f633255847499b00009ad52c
Gilad Even-Tov <gilad@eventov.com>
2020-12-13 16:51:21 +0200

    Add sentiment and answer endpoints

--- app/api.py
+++ app/api.py
@@ -9,10 +9,28 @@ def api_index():
 
 @app.route("/api/sentiments/", methods=["GET"])
 def sentiments_index():
-    data = { "PING": "PONG" }
-    return make_response(jsonify(data), 200)
+    tweet = Tweet.query.filter(~Tweet.answers.any()).first()
+
+    if tweet != None:
+        data = { "id": tweet.id, "text": tweet.text }
+        return make_response(jsonify(data), 200)
+    else:
+        data = { "id": None, "text": None }
+        return make_response(jsonify(data), 200)
 
 @app.route("/api/answers/<tweetid>", methods=["POST"])
 def post_answer(tweetid):
-    data = { "PING": "PONG" }
-    return make_response(jsonify(data), 200)
+    tweet = Tweet.query.filter_by(id=tweetid).first()
+    answer = Answer(tweet, SentimentType[request.json['sentiment']])
+
+    db.session.add(answer)
+    db.session.commit()
+
+    tweet = Tweet.query.filter(~Tweet.answers.any()).first()
+
+    if tweet != None:
+        data = { "id": tweet.id, "text": tweet.text }
+        return make_response(jsonify(data), 200)
+    else:
+        data = { "id": None, "text": None }
+        return make_response(jsonify(data), 200)
