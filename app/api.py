2c0510f24979522526f57b1c580ceb31b1e9a59d
Gilad Even-Tov <gilad@eventov.com>
2020-12-13 16:11:17 +0200

    Start additional api endpoints

--- app/api.py
+++ app/api.py
@@ -1,7 +1,18 @@
-from flask import jsonify, make_response
+from flask import request, jsonify, make_response
 from app import app
+from app.models import *
 
-@app.route("/api/", methods=["ANY"])
+@app.route("/api/", methods=["GET"])
 def api_index():
+    data = { "PING": "PONG" }
+    return make_response(jsonify(data), 200)
+
+@app.route("/api/sentiments/", methods=["GET"])
+def sentiments_index():
+    data = { "PING": "PONG" }
+    return make_response(jsonify(data), 200)
+
+@app.route("/api/answers/<tweetid>", methods=["POST"])
+def post_answer(tweetid):
     data = { "PING": "PONG" }
     return make_response(jsonify(data), 200)
